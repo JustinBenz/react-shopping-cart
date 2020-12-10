d445ad1df829674e9397025a3c528ab3761db69b
Justin Benz <jbenz156@gmail.com>
2020-12-10 16:37:35 -0500

    added productContext.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,12 +7,16 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import productContext from "./contexts/productContext"
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart(item);
 	};
 
 	return (
